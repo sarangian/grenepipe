d65e277d550560a55725045dd9c8b9116ba3d109
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2024-08-26 21:47:59 +0530

    Update hafpipe.py

--- workflow/scripts/hafpipe.py
+++ workflow/scripts/hafpipe.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/opt/conda/envs/grenepipe/bin/python3
 
 # =================================================================================================
 #     Wrapper script to run HAFpipe in Snakemake
